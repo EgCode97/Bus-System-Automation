a39682cb9c25a4055a1ec2cdb980b29d1054be13
EgCode97 <eliasgerardoc@gmail.com>
2020-06-28 00:04:52 -0400

    creation of the project folder structure

---
+++ Bus_Stop_Script/Bluetooth_master.py
@@ -0,0 +1,5 @@
+# Get the transmited data from buses and upload it to server with the API
+# 1- Scan devices
+# 2- Request for bus ID
+# 3- Break connection
+# 4- Upload data via API
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Bus-System-Automation
 A web based monitoring system for bus based public transport
+
+"Bus_automation" folder contains the django website and API
