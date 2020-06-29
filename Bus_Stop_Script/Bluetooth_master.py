17747d36c6b04d7bad9c5cc5d53712b3933eb0a1
EgCode97 <eliasgerardoc@gmail.com>
2020-06-29 00:03:57 -0400

    Bluetooth interface added. Succesfull communication between bus and bus stop

--- Bus_Stop_Script/Bluetooth_master.py
+++ Bus_Stop_Script/Bluetooth_master.py
@@ -1,5 +1,118 @@
 # Get the transmited data from buses and upload it to server with the API
 # 1- Scan devices
-# 2- Request for bus ID
-# 3- Break connection
-# 4- Upload data via API
+# 2- Connect to device
+# 3- Make sure it's a valid device sending the verification command (A) must receive and "A" after sending
+# IF VALID DEVICES GO ON... ELSE SKIP TO BREAK CONNECTION AND AND SCAN AGAIN
+# 4- Request for bus ID
+# 5- Break connection
+# 6- Upload data via API
+
+import serial
+import serial.tools.list_ports
+import time
+
+#---------------------------------------------------------------------------------
+#   Blueeth module commands
+COMMAND_SCAN = "AT+INQ\r\n" # Scan for slave blueetooth devices -> returns the scanned devices with a number and MAC address
+COMMAND_CONNECT = "AT+CONN*\r\n" # 
+
+COMMAND_GET_ID = "B"
+COMMAND_DISCONNECT = chr(0)
+
+class BusStop:
+    def __init__(self):
+        self.connected_devices = list()
+        self.checked_bus = list()
+        self.comport = self.get_comport()
+
+    def get_comport(self):
+        'Get the comport in wich the Bluetooth module is connected. Returns None if no module is connected'
+        for port in [comport.device for comport in serial.tools.list_ports.comports()]:
+            comport = serial.Serial(port, baudrate= 9600, timeout = 0.25)
+
+            comport.write(b'AT\r\n')
+            if not comport.read(4):
+                comport.close()
+            else:
+                return comport
+
+    def receive(self, bytes_num : int = 1) -> str:
+        'Receive a specific amount of bytes and the return a string, if bytes num is omited then is taken as 1'
+        if self.comport:
+            return self.comport.read(bytes_num).decode()
+
+    def send(self, message : str) -> None:
+        if self.comport:
+            self.comport.write(message.encode())
+
+    def scan_devices(self) -> dict:
+        'Scan all the slave devices in range and returns a dict with items as "scan_number":"device_mac"'
+        bus_stop.send(COMMAND_SCAN)
+        scan = str()
+        while not scan.endswith("INQE"):
+            scan += bus_stop.receive()
+        
+        scan_list = scan.split('\r\n')
+        scan_list.pop(0)
+        scan_list.pop(-1)
+
+        scanned_dict = dict()
+        for device in scan_list:
+            try:
+                device_number, device_mac = device.split("=")   # returns a ["+INQ:1", "MAC ADDRESS"]
+                scanned_dict[device_number[-1]] = device_mac
+            except:
+                pass
+            
+        return scanned_dict
+
+    def connect_device(self, scan_num : str):
+        print(f"CONNECTING {scan_num}")
+        self.send( COMMAND_CONNECT.replace('*', scan_num) )
+        response = str()
+        while not "+Connected\r\n" in response:
+            response += self.receive()
+        print(response)
+        return response
+
+        
+
+
+
+
+bus_stop = BusStop()
+while True:
+    scan = bus_stop.scan_devices()
+    #scan['8'] ='0x508CB15175E9'
+
+    print(f"SCAN -> {scan}")
+
+    for device in scan:
+        device_mac = scan[device]
+
+        if device_mac in bus_stop.connected_devices:
+            print(f"DEVICE '{device}' HAVE ALREDY BEEN CONNECTED")
+            continue
+
+        bus_stop.connect_device(device)
+
+        time.sleep(5)
+        print("CONECTADO")
+        bus_stop.connected_devices.append(device_mac)
+
+
+        bus_stop.send( "B" )
+        time.sleep(0.1)
+        bus_id = bus_stop.receive(20)
+
+        print(f"ID -> {bus_id.encode()}")
+
+
+        time.sleep(0.1)
+        bus_stop.send( COMMAND_DISCONNECT )
+        print("DESCONECTADO\n")
+        time.sleep(30)
+
+
+
+        print(bus_stop.connected_devices)
--- Bus_automation/Bus_automation/settings.py
+++ Bus_automation/Bus_automation/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from secret_key import SECRET_KEY as SK
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#6g626j-bn$_!thka*iex4$)iqxnn^l538#h^s4ab8v*n46_10'
+SECRET_KEY = SK
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
