1290abba32dc6153932eb219b96ca3e99e6a6ae0
EgCode97 <eliasgerardoc@gmail.com>
2020-07-21 23:01:21 -0400

    added filters and created RegisterAdmin. Included register model in adminpage

--- Bus_automation/API/admin.py
+++ Bus_automation/API/admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
-from .models import BusStop, BusUnit, Route, Zone
+from .models import BusStop, BusUnit, Route, Zone, Register
 # Register your models here.
 
 class BusStopAdmin(admin.ModelAdmin):
     list_display = ('name', 'address', 'zone')
+    list_filter = ('zone',)
 
 class BusUnitAdmin(admin.ModelAdmin):
     list_display = ('id', 'route', 'location')
+    list_filter = ('route',)
 
 
 class RouteAdmin(admin.ModelAdmin):
@@ -17,9 +19,15 @@ class RouteAdmin(admin.ModelAdmin):
     'stop16', 'stop17', 'stop18', 'stop19', 'stop20',)
 
 class ZoneAdmin(admin.ModelAdmin):
-    list_display = ('name',)
+    list_display = ('name', 'description')
+
+class RegisterAdmin(admin.ModelAdmin):
+    list_display = ('id', 'location', 'unit', 'datetime')
+    list_filter = ('location', 'unit', 'datetime')
+
 
 admin.site.register(BusStop, BusStopAdmin)
 admin.site.register(BusUnit, BusUnitAdmin)
 admin.site.register(Route, RouteAdmin)
 admin.site.register(Zone, ZoneAdmin)
+admin.site.register(Register, RegisterAdmin)
