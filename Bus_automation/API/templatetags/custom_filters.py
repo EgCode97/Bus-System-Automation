be20bb67d6927b38f3952e744b893e43e0b7d2ca
EgCode97 <eliasgerardoc@gmail.com>
2020-07-26 18:35:03 -0400

    added customs filters to return month name given the month number and to return the minutes as a two digits value

--- Bus_automation/API/templatetags/custom_filters.py
+++ Bus_automation/API/templatetags/custom_filters.py
@@ -9,3 +9,27 @@ def is_even(num):
         return 1 if num % 2 == 0 else 0
     except ZeroDivisionError:
         return 0
+
+@register.filter(name='month_as_str')
+def month_as_str(month_num):
+    months = {
+        1: 'Enero',
+        2: 'Febrero',
+        3: 'Marzo',
+        4: 'Abril',
+        5: 'Mayo',
+        6: 'Junio',
+        7: 'Julio',
+        8: 'Agosto',
+        9: 'Septiembre',
+        10: 'Octubre',
+        11: 'Noviembre',
+        12: 'Diciembre',
+    }
+    return months[month_num]
+
+
+@register.filter(name='minutes_two_digits')
+def minutes_two_digits(minute):
+    minute = str(minute)
+    return minute if len(minute) > 1 else f"0{minute}"
