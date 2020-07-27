b726e772750fd8aba84dc900eb29b4a73935412f
EgCode97 <eliasgerardoc@gmail.com>
2020-07-27 16:34:01 -0400

    heroku settings

--- Bus_automation/Bus_automation/settings.py
+++ Bus_automation/Bus_automation/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
-
+import django_heroku
 from corsheaders.defaults import default_headers
 
 CORS_ALLOW_HEADERS = default_headers + (
@@ -143,3 +143,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+django_heroku.settings(locals())
